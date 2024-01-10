ebee6a6189541a8fea6e32715478e1bc5dab7177
Rool Paap <rool@rool.nl>
2024-01-10 15:02:43 +0100

    Align the rijkslint to the notch or dynamic island

--- Sources/MDRF/LaunchView.swift
+++ Sources/MDRF/LaunchView.swift
@@ -8,19 +8,54 @@
 import SwiftUI
 
 struct LaunchView: View {
+	
+	@State var rijkslintTopOffset: CGFloat = 0
+	
+	private struct ViewTraits {
+		
+		static let dynamicIslandHeight: CGFloat = 59
+		static let dynamicIslandOffset: CGFloat = 11
+		static let notchHeight: CGFloat = 47
+		static let notchOffset: CGFloat = 13
+		
+		enum Title {
+			static let topOffset: CGFloat = 64
+		}
+	}
+	
+	func recalculateOffset(safeAreaHeight: CGFloat) {
+		if safeAreaHeight >= ViewTraits.dynamicIslandHeight {
+			rijkslintTopOffset = safeAreaHeight - ViewTraits.dynamicIslandOffset
+		} else if safeAreaHeight >= ViewTraits.notchHeight {
+			rijkslintTopOffset = safeAreaHeight - ViewTraits.notchOffset
+		}
+		print("Safe area height: \(safeAreaHeight) -> \(rijkslintTopOffset)")
+	}
+	
 	var body: some View {
-		ZStack {
-			Color.background
-				.ignoresSafeArea()
-				.frame(maxWidth: .infinity, maxHeight: .infinity)
-			VStack {
-				Image(ImageResource.rijkslint)
+		GeometryReader { geometry in
+			ZStack {
+				Color.background
 					.ignoresSafeArea()
-				Text("launch_title")
-					.font(.largeTitle)
-					.fontWeight(.bold)
-					.foregroundColor(Color.splashTitle)
-				Spacer()
+					.frame(maxWidth: .infinity, maxHeight: .infinity)
+				VStack {
+					Image(ImageResource.rijkslint)
+						.padding(.top, rijkslintTopOffset)
+						.ignoresSafeArea()
+					
+					Text("launch_title")
+						.font(.largeTitle)
+						.fontWeight(.bold)
+						.foregroundColor(Color.splashTitle)
+						.padding(.top, ViewTraits.Title.topOffset - rijkslintTopOffset)
+					Spacer()
+				}
+				.onAppear {
+					recalculateOffset(safeAreaHeight: geometry.safeAreaInsets.top)
+				}
+				//	.onReceive(NotificationCenter.default.publisher(for: UIDevice.orientationDidChangeNotification)) { _ in
+				//	recalculateOffset(safeAreaHeight: geometry.safeAreaInsets.top)
+				//	}
 			}
 		}
 	}
