2d195da9c24f9ae5a08ece12ac6d241e6810a227
Rool Paap <rool@rool.nl>
2024-01-10 15:13:42 +0100

    Fixed the position of the rijkslint on rotation

--- Sources/MDRF/LaunchView.swift
+++ Sources/MDRF/LaunchView.swift
@@ -12,24 +12,29 @@ struct LaunchView: View {
 	@State var rijkslintTopOffset: CGFloat = 0
 	
 	private struct ViewTraits {
-		
-		static let dynamicIslandHeight: CGFloat = 59
-		static let dynamicIslandOffset: CGFloat = 11
-		static let notchHeight: CGFloat = 47
-		static let notchOffset: CGFloat = 13
-		
+		enum DynamicIsland {
+			static let height: CGFloat = 59
+			static let offset: CGFloat = 11
+		}
+		enum Notch {
+			static let height: CGFloat = 47
+			static let offset: CGFloat = 13
+		}
 		enum Title {
 			static let topOffset: CGFloat = 64
 		}
 	}
 	
+	/// Calculate the offset for the rijkslint so it stays just below the notch or dynamic island
+	/// - Parameter safeAreaHeight: the height of the safe area
 	func recalculateOffset(safeAreaHeight: CGFloat) {
-		if safeAreaHeight >= ViewTraits.dynamicIslandHeight {
-			rijkslintTopOffset = safeAreaHeight - ViewTraits.dynamicIslandOffset
-		} else if safeAreaHeight >= ViewTraits.notchHeight {
-			rijkslintTopOffset = safeAreaHeight - ViewTraits.notchOffset
+		if safeAreaHeight >= ViewTraits.DynamicIsland.height {
+			rijkslintTopOffset = safeAreaHeight - ViewTraits.DynamicIsland.offset
+		} else if safeAreaHeight >= ViewTraits.Notch.height {
+			rijkslintTopOffset = safeAreaHeight - ViewTraits.Notch.offset
+		} else {
+			rijkslintTopOffset = 0
 		}
-		print("Safe area height: \(safeAreaHeight) -> \(rijkslintTopOffset)")
 	}
 	
 	var body: some View {
@@ -53,9 +58,9 @@ struct LaunchView: View {
 				.onAppear {
 					recalculateOffset(safeAreaHeight: geometry.safeAreaInsets.top)
 				}
-				//	.onReceive(NotificationCenter.default.publisher(for: UIDevice.orientationDidChangeNotification)) { _ in
-				//	recalculateOffset(safeAreaHeight: geometry.safeAreaInsets.top)
-				//	}
+				.onChange(of: geometry.safeAreaInsets.top) { _ in
+					recalculateOffset(safeAreaHeight: geometry.safeAreaInsets.top)
+				}
 			}
 		}
 	}
