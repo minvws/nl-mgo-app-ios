23e00234375a2f28b05914aa987df157d3c9ede3
Rool Paap <rool@rool.nl>
2024-01-25 15:54:43 +0100

    chore: Test Package
    So we can do import GifzTest instead of import XCTest, import Nimble, import Snapshot

---
+++ Packages/GifzTest/Sources/GifzTest/GifzTest.swift
@@ -0,0 +1,13 @@
+/*
+ *  Copyright (c) 2024 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
+ *  Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
+ *
+ *  SPDX-License-Identifier: EUPL-1.2
+ */
+
+// System
+@_exported import XCTest
+
+// External
+@_exported import Nimble
+@_exported import SnapshotTesting
