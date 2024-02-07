1a186e025319e629ac0d9665fa904b08880c1f57
Rool Paap <rool@rool.nl>
2024-02-07 11:35:30 +0100

    feat(introduction): Updated dependencies

--- Packages/MGOFoundation/Package.swift
+++ Packages/MGOFoundation/Package.swift
@@ -15,17 +15,13 @@ let package = Package(
 	dependencies: [
 		
 		// VWS
-		.package(url: "https://github.com/minvws/nl-rdo-app-ios-modules", branch: "main"),
-		
-		// External
-		.package(url: "https://github.com/lm/navigation-stack-backport", from: "1.1.0")
+		.package(url: "https://github.com/minvws/nl-rdo-app-ios-modules", branch: "main")
 	],
 	targets: [
 		.target(
 			name: "MGOFoundation",
 			dependencies: [
-				.product(name: "Logging", package: "nl-rdo-app-ios-modules"),
-				.product(name: "NavigationStackBackport", package: "navigation-stack-backport")
+				.product(name: "Logging", package: "nl-rdo-app-ios-modules")
 			]
 		)
 	]
