107f8d9d7378d93b2ad7552e790b4339ac0c0e91
Rool Paap <rool@rool.nl>
2024-02-16 11:33:27 +0100

    feat(reset): Added configuration and release

---
+++ Sources/MGO/Core/Configuration/Release.swift
@@ -0,0 +1,16 @@
+/*
+ *  Copyright (c) 2024 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
+ *  Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
+ *
+ *  SPDX-License-Identifier: EUPL-1.2
+ */
+
+import MGOFoundation
+
+/// The flavours of the app
+public enum Release: String {
+	case production
+	case acceptance = "acc"
+	case test
+	case development = "dev"
+}
