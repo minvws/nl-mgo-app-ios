7bbfc270e1bdb4c057963152610850cd7471fe07
Rool Paap <rool@rool.nl>
2024-02-22 14:28:34 +0100

    feat(FHIR Client): Convert package to Swift 5.9

--- Packages/Swift-SMART/Package.swift
+++ Packages/Swift-SMART/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version: 5.9
 //  Package.swift
 //
 //  Created by Dave Carlson on 8/8/19.
@@ -8,25 +8,28 @@ import PackageDescription
 let package = Package(
     name: "SMART",
 	platforms: [
-		.macOS(.v10_13), .iOS(.v11)
+		.macOS(.v10_13),
+		.iOS(.v12)
 	],
-    products: [
-        .library(
-            name: "SMART",
-            targets: ["SMART"]),
-    ],
-    dependencies: [
-		.package(url: "https://github.com/smart-on-fhir/Swift-FHIR", "4.2.0"..<"5.0.0"),
-		.package(url: "https://github.com/p2/OAuth2", "5.1.0"..<"6.0.0"),
-    ],
-    targets: [
+	products: [
+		.library(
+			name: "SMART",
+			targets: ["SMART"]
+		)
+	],
+	dependencies: [
+		.package(name: "Swift-FHIR", path: "../Swift-FHIR"),
+//		.package(url: "https://github.com/smart-on-fhir/Swift-FHIR", exact: "3.1.1"),
+		.package(url: "https://github.com/p2/OAuth2", exact: "5.3.3")
+	],
+	targets: [
 		.target(
 			name: "SMART",
 			dependencies: [
-				.product(name: "OAuth2"),
-				.product(name: "FHIR"),
+				.product(name: "OAuth2", package: "OAuth2"),
+				.product(name: "FHIR", package: "Swift-FHIR")
 			],
 			path: "Sources",
 			sources: ["SMART", "Client", "iOS", "macOS"]),
-    ]
+	]
 )
