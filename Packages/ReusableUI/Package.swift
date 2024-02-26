8761f27d0d9dccdc8be45824c82d2be999fc4cd9
Rool Paap <rool@rool.nl>
2024-02-26 08:47:09 +0100

    chore: updated Nimble to 13.2.1

--- Packages/ReusableUI/Package.swift
+++ Packages/ReusableUI/Package.swift
@@ -16,7 +16,7 @@ let package = Package(
 		
 		// Testing:
 		.package(url: "https://github.com/pointfreeco/swift-snapshot-testing", exact: "1.15.3"),
-		.package(url: "https://github.com/Quick/Nimble", exact: "13.2.0"),
+		.package(url: "https://github.com/Quick/Nimble", exact: "13.2.1"),
 		.package(url: "https://github.com/nalexn/ViewInspector", exact: "0.9.10")
 	],
 	targets: [
