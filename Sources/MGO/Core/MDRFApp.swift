2b5882a1c49801885603cba750672b95ee302d32
Rool Paap <rool@rool.nl>
2024-02-28 12:41:31 +0100

    test: Added Model Client tests 4/4

--- Sources/MGO/Core/MDRFApp.swift
+++ Sources/MGO/Core/MDRFApp.swift
@@ -34,6 +34,8 @@ struct ProductionApp: App {
 	var body: some Scene {
 		WindowGroup {
 			AppCoordinatorView<AppCoordinator>(appCoordinator: AppCoordinator(path: NavigationStackBackport.NavigationPath()))
+			// POC FHIR CLient
+//			PatientView(viewModel: PatientViewModel())
 		}
 	}
 }
