ac1251138a494f26790d3ccaca8d74bc5aa679ea
Rool Paap <rool@rool.nl>
2024-03-05 13:31:25 +0100

    chore: fix copyright

--- Packages/FHIRClient/Sources/FHIRClient/Client/Request/RequestHandler.swift
+++ Packages/FHIRClient/Sources/FHIRClient/Client/Request/RequestHandler.swift
@@ -1,10 +1,9 @@
-//
-//  FHIRRequestHandler.swift
-//  SwiftFHIR
-//
-//  Created by Pascal Pfiffner on 3/31/15.
-//  2015, SMART Platforms.
-//
+/*
+ *  Copyright (c) 2024 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
+ *  Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
+ *
+ *  SPDX-License-Identifier: EUPL-1.2
+ */
 
 import Foundation
 
