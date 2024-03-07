13392243f0909f1ae7ea971a9d6aaa9632bb1b25
Rool Paap <rool@rool.nl>
2024-03-07 12:29:58 +0100

    feat(local authentication): Added biometric type

---
+++ Sources/MGO/Features/LocalAuthentication/Extensions/LocalAuthentication+BiometricType.swift
@@ -0,0 +1,47 @@
+/*
+ *  Copyright (c) 2024 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
+ *  Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
+ *
+ *  SPDX-License-Identifier: EUPL-1.2
+ */
+
+import LocalAuthentication
+import MGOFoundation
+
+enum LocalAuthentication {
+	
+	/// The various Biometric types
+	enum BiometricType: String {
+		case none
+		case touchID
+		case faceID
+		case opticID
+		case unknown
+	}
+}
+
+extension LAContext {
+	
+	/// Get the biometric Type for this device
+	var biometricType: LocalAuthentication.BiometricType {
+		var error: NSError?
+
+		guard self.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) else {
+			logError("MGO: Can't evaluate LAPolicy", error?.localizedDescription ?? "")
+			return .none
+		}
+		
+		switch self.biometryType {
+			case .none:
+				return .none
+			case .touchID:
+				return .touchID
+			case .faceID:
+				return .faceID
+			case .opticID:
+				return .opticID
+			@unknown default:
+				return .unknown
+		}
+	}
+}
