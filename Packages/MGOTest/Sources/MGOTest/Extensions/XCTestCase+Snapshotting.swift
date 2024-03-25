dbb2d082d2ce543f944cde0180305064129c5725
Rool Paap <rool@rool.nl>
2024-03-25 12:33:11 +0100

    feat: added precision for snapshots

--- Packages/MGOTest/Sources/MGOTest/Extensions/XCTestCase+Snapshotting.swift
+++ Packages/MGOTest/Sources/MGOTest/Extensions/XCTestCase+Snapshotting.swift
@@ -23,12 +23,12 @@ extension XCTestCase {
 	/// - Parameters:
 	///   - content: the view for the snapshots
 	///   - name: The name of the test
-	public func takeSnapShots(content: some View, name: String, file: StaticString = #file) {
+	public func takeSnapShots(content: some View, name: String, precision: Float = 1.0, file: StaticString = #file) {
 		
 		// Dark Mode
 		assertSnapshot(
 			of: UIHostingController(rootView: content.colorScheme(.dark)),
-			as: .image,
+			as: .image(precision: precision),
 			named: "_darkPortrait",
 			file: file,
 			testName: name
@@ -37,7 +37,7 @@ extension XCTestCase {
 		// Light Mode
 		assertSnapshot(
 			of: UIHostingController(rootView: content.colorScheme(.light)),
-			as: .image,
+			as: .image(precision: precision),
 			named: "_lightPortrait",
 			file: file,
 			testName: name
@@ -45,7 +45,7 @@ extension XCTestCase {
 		
 		assertSnapshot(
 			of: UIHostingController(rootView: content.colorScheme(.dark)),
-			as: .image(on: .iPhone15Pro(.landscape)),
+			as: .image(on: .iPhone15Pro(.landscape), precision: precision),
 			named: "_darkLandscape",
 			file: file,
 			testName: name
@@ -53,7 +53,7 @@ extension XCTestCase {
 		
 		assertSnapshot(
 			of: UIHostingController(rootView: content.colorScheme(.light)),
-			as: .image(on: .iPhone15Pro(.landscape)),
+			as: .image(on: .iPhone15Pro(.landscape), precision: precision),
 			named: "_lightLandscape",
 			file: file,
 			testName: name
