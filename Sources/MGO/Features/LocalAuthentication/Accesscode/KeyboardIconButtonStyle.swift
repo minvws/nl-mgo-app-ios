9a838cee1895ee461772a84bc7729bdb19788dda
Rool Paap <rool@rool.nl>
2024-04-17 10:31:37 +0200

    style: changed the backspace icon

---
+++ Sources/MGO/Features/LocalAuthentication/Accesscode/KeyboardIconButtonStyle.swift
@@ -0,0 +1,50 @@
+/*
+ *  Copyright (c) 2024 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
+ *  Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
+ *
+ *  SPDX-License-Identifier: EUPL-1.2
+ */
+
+import RijksoverheidFont
+import SwiftUI
+import Theme
+
+struct KeyboardIconButtonStyle: ButtonStyle {
+
+	/// Color scheme (light, dark)
+	@Environment(\.colorScheme) var colorScheme
+	
+	/// Is the button enabled?
+	@Environment(\.isEnabled) private var isEnabled: Bool
+	
+	/// The Theme
+	@Environment(\.theme) var theme
+	
+	/// Magic Numbers
+	private struct ViewTraits {
+		enum Button {
+			static let minimumHeight: CGFloat = 44
+		}
+		enum Circle {
+			static let size: CGFloat = 50
+		}
+	}
+	
+	/// Style the button to a primary button
+	/// - Parameter configuration: the button configuration
+	/// - Returns: primary button
+	func makeBody(configuration: Self.Configuration) -> some View {
+		
+		configuration.label
+			.frame(maxWidth: .infinity, minHeight: ViewTraits.Button.minimumHeight)
+			.rijksoverheidStyle(font: .regular, style: .title)
+			.foregroundStyle(isEnabled ? theme.iconsPrimary : theme.iconsPrimary.opacity(0.25))
+			.background {
+				if configuration.isPressed {
+					Circle()
+						.foregroundStyle(theme.backgroundTertiary)
+						.frame(width: ViewTraits.Circle.size, height: ViewTraits.Circle.size)
+				}
+			}
+	}
+}
