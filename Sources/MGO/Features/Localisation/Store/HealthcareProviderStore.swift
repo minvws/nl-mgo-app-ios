32afe23c88d22108d9af4b3662bc613f64fd9a0c
Rool Paap <rool@rool.nl>
2024-05-01 12:50:48 +0200

    feat(MGO-350): StoredHealthcare providers on disk

--- Sources/MGO/Features/Localisation/Store/HealthcareProviderStore.swift
+++ Sources/MGO/Features/Localisation/Store/HealthcareProviderStore.swift
@@ -10,33 +10,76 @@ import LocalisationServiceClient
 
 protocol HealthcareProviderStoreProtocol {
 	
+	/// Add a healthcare provider to the storage
+	/// - Parameter provider: the healthcare provider to store
 	func store(_ provider: HealthcareProvider) throws
 	
+	/// Get a list of all the stored healthcare providers
+	/// - Returns: array of healthcare providers
 	func read() async throws -> [HealthcareProvider]
 	
+	/// Delete a healthcare provider from storage
+	/// - Parameter provider: the healthcare provider to be removed
 	func remove(_ provider: HealthcareProvider) throws
+	
+	/// Remove all the healthcare providers
+	func wipe()
 }
 
 class HealthcareProviderStore: HealthcareProviderStoreProtocol {
 	
 	private let queue = DispatchQueue(label: "com.healthcareProviderStore.serialqueue.\(UUID().uuidString)")
 	
-	private var list = [HealthcareProvider]()
+	private let storage = FileStorage()
 	
+	private let fileName = "healthcareproviders.json"
+	
+	/// Add a healthcare provider to the storage
+	/// - Parameter provider: the healthcare provider to store
 	func store(_ provider: HealthcareProvider) throws {
 		
-		queue.sync {
+		try queue.sync {
+			
+			var list = [HealthcareProvider]()
+			
+			if let jsonData = storage.read(fileName: fileName) {
+				let data = try JSONDecoder().decode([HealthcareProvider].self, from: jsonData)
+				list = data
+			}
 			list.append(provider)
-		}}
-	
-	func read() async throws -> [HealthcareProvider] {
-		return list
+			let encoded = try JSONEncoder().encode(list)
+			try storage.store(encoded, as: fileName)
+		}
 	}
 	
+	/// Get a list of all the stored healthcare providers
+	/// - Returns: array of healthcare providers
+	func read() async throws -> [HealthcareProvider] {
+		
+		if let jsonData = storage.read(fileName: fileName) {
+			let data = try JSONDecoder().decode([HealthcareProvider].self, from: jsonData)
+			return data
+		}
+		return []
+	}
+	
+	/// Delete a healthcare provider from storage
+	/// - Parameter provider: the healthcare provider to be removed
 	func remove(_ provider: HealthcareProvider) throws {
 		
-		queue.sync {
-			list = list.filter { $0 != provider }
+		var list = [HealthcareProvider]()
+		
+		if let jsonData = storage.read(fileName: fileName) {
+			let data = try JSONDecoder().decode([HealthcareProvider].self, from: jsonData)
+			list = data
 		}
+		list = list.filter { $0 != provider }
+		let encoded = try JSONEncoder().encode(list)
+		try storage.store(encoded, as: fileName)
+	}
+	
+	/// Remove all the healthcare providers
+	func wipe() {
+		storage.remove(fileName)
 	}
 }
