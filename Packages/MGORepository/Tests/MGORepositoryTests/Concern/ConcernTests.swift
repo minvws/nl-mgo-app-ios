9a7ebde05457f8e2f768194245925f99b963a5ee
Rool Paap <rool@rool.nl>
2024-06-03 16:09:00 +0200

    feat(MGO-448): More decorator tests

--- Packages/MGORepository/Tests/MGORepositoryTests/Concern/ConcernTests.swift
+++
@@ -1,8 +0,0 @@
-@testable import MGORepository
-import XCTest
-
-final class ConcernTests: XCTestCase {
-    func test_zero() throws {
-        XCTFail("Tests not yet implemented in Tests")
-    }
-}
