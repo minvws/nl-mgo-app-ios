c9b56afff70c061cc4052db1acd296822984b562
Rool Paap <rool@rool.nl>
2024-06-06 08:36:14 +0200

    fix: Fixed import

--- Sources/MGO/Core/LaunchArgumentsHandler.swift
+++ Sources/MGO/Core/LaunchArgumentsHandler.swift
@@ -1,11 +1,11 @@
 /*
-*  Copyright (c) 2024 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
-*  Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
-*
-*  SPDX-License-Identifier: EUPL-1.2
-*/
+ *  Copyright (c) 2024 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
+ *  Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
+ *
+ *  SPDX-License-Identifier: EUPL-1.2
+ */
 
-import UIKit
+import Foundation
 
 public class LaunchArgumentsHandler {
 	
