d71b0773914c8dcd78bd0605164744164be0ac32
Rool Paap <rool@rool.nl>
2024-06-24 16:04:31 +0200

    refactor: avoided nested closures.

--- Packages/ReusableUI/Sources/ReusableUI/IPadLayoutViewModifier.swift
+++ Packages/ReusableUI/Sources/ReusableUI/IPadLayoutViewModifier.swift
@@ -32,16 +32,16 @@ public struct IPadLayoutViewModifier: ViewModifier {
 	public func body(content: Content) -> some View {
 		
 		content
-		.if((shouldLayoutForiPad && horizontalSizeClass == .regular) || force) { view in
-			GeometryReader { geometry in
-				HStack(spacing: 0, content: {
-					Spacer(minLength: geometry.size.width * ViewTraits.Gutter.leading)
-					view
-					Spacer(minLength: geometry.size.width * ViewTraits.Gutter.trailing)
+			.if((shouldLayoutForiPad && horizontalSizeClass == .regular) || force, transform: { view in
+				GeometryReader(content: {geometry in
+					HStack(spacing: 0, content: {
+						Spacer(minLength: geometry.size.width * ViewTraits.Gutter.leading)
+						view
+						Spacer(minLength: geometry.size.width * ViewTraits.Gutter.trailing)
+					})
+					.background(theme.backgroundPrimary.ignoresSafeArea())
 				})
-				.background(theme.backgroundPrimary.ignoresSafeArea())
-			}
-		}
+			})
 	}
 }
 
