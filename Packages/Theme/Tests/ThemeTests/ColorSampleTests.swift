ec65ccbd4c6ee6a4cbe959513fa84aa90e9e6123
Rool Paap <rool@rool.nl>
2024-08-15 15:34:15 +0200

    Tests: Updated snapshots with updated colors

--- Packages/Theme/Tests/ThemeTests/ColorSampleTests.swift
+++ Packages/Theme/Tests/ThemeTests/ColorSampleTests.swift
@@ -17,7 +17,7 @@ final class ColorSampleTests: XCTestCase {
 		let sut = ColorSampleView()
 		
 		// When
-		let content = sut.frame(width: 380, height: 2100)
+		let content = sut.frame(width: 380, height: 2500)
 		
 		// Then
 		assertSnapshot(of: UIHostingController(rootView: content.colorScheme(.light)), as: .image)
@@ -29,7 +29,7 @@ final class ColorSampleTests: XCTestCase {
 		let sut = ColorSampleView()
 		
 		// When
-		let content = sut.frame(width: 380, height: 2100)
+		let content = sut.frame(width: 380, height: 2500)
 		
 		// Then
 		assertSnapshot(of: UIHostingController(rootView: content.colorScheme(.dark)), as: .image)
