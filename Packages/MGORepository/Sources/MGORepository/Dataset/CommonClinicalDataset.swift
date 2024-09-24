bc9414d76741c651159e40619f60ff08065091a9
Rool Paap <rool@rool.nl>
2024-09-24 15:27:08 +0200

    fix: Revert temp fix for funcionalOrMentalStatus

--- Packages/MGORepository/Sources/MGORepository/Dataset/CommonClinicalDataset.swift
+++ Packages/MGORepository/Sources/MGORepository/Dataset/CommonClinicalDataset.swift
@@ -79,8 +79,7 @@ extension DVP {
 			path: "Observation",
 			parameters: RequestParameters(
 				[
-//					(RequestParameterField.category, "http://snomed.info/sct|118228005,http://snomed.info/sct|384821006")
-					(RequestParameterField.category, "http://snomed.info/sct|11291000146105,http://snomed.info/sct|384821006")
+					(RequestParameterField.category, "http://snomed.info/sct|118228005,http://snomed.info/sct|384821006")
 				]
 			),
 			directory: lastn
