65f46baa0ff5172f52475c64beb4a2344e717055
Rool Paap <rool@rool.nl>
2024-10-04 16:41:10 +0200

    fix: fixed typo

--- MGOTests/Features/HealthCategory/HealthCategoryViewModelTests.swift
+++ MGOTests/Features/HealthCategory/HealthCategoryViewModelTests.swift
@@ -237,7 +237,7 @@ final class HealthCategoryViewModelTests: XCTestCase {
 		
 		// Then
 		expect(self.servicesSpies.dataStoreSpy.invokedRemoveRecordsFor) == true
-		expect(self.servicesSpies.resourceRepositorySpy.invokedLoadResourceCount).toEventually(equal(1), timeout: .seconds(5)
+		expect(self.servicesSpies.resourceRepositorySpy.invokedLoadResourceCount).toEventually(equal(1), timeout: .seconds(5))
 	}
 	
 	func test_retry_noServices() {
@@ -263,7 +263,7 @@ final class HealthCategoryViewModelTests: XCTestCase {
 		
 		// Then
 		expect(self.servicesSpies.dataStoreSpy.invokedRemoveRecordsFor) == true
-		expect(self.servicesSpies.resourceRepositorySpy.invokedLoadForHealthCategoriesCategoryCount).toEventually(equal(1), timeout: .seconds(5)
+		expect(self.servicesSpies.resourceRepositorySpy.invokedLoadForHealthCategoriesCategoryCount).toEventually(equal(1), timeout: .seconds(5))
 	}
 	
 	func test_handleDataStoreChanges() {
