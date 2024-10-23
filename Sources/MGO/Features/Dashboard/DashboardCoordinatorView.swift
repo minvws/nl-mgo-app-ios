c4be1c1189bfde3d83973d7d0eabc539409b9fd7
Rool Paap <rool@rool.nl>
2024-10-23 09:01:13 +0200

    style: fixed navigation divider when scrolling

--- Sources/MGO/Features/Dashboard/DashboardCoordinatorView.swift
+++ Sources/MGO/Features/Dashboard/DashboardCoordinatorView.swift
@@ -467,6 +467,10 @@ struct DashboardCoordinatorView<T: DashboardCoordinatorProtocol>: View {
 					.accessibilityIdentifier("bottombar.about_this_app")
 				}
 				.tint(theme.actionTertiaryDefaultText)
+				.onPreferenceChange(IsScrollingPreferenceKey.self, perform: { newValue in
+//					_ = logVerbose("ACV isScrolling: \(newValue.last ?? false)")
+					isScrolling = newValue.last ?? false
+				})
 			}
 			.onAppear(perform: {
 				// Brute force styling
@@ -494,14 +498,9 @@ struct DashboardCoordinatorView<T: DashboardCoordinatorProtocol>: View {
 				// Apply
 				UITabBar.appearance().standardAppearance = tabBarAppearance
 				UITabBar.appearance().scrollEdgeAppearance = tabBarAppearance
-
 			})
 			.navigationBarHidden(true)
 			.navigationBarBackButtonHidden()
-			.onPreferenceChange(IsScrollingPreferenceKey.self, perform: { newValue in
-				_ = logVerbose("ACV isScrolling: \(newValue.last ?? false)")
-				isScrolling = newValue.last ?? false
-			})
 			.inspectableSheet(
 				isPresented: $coordinator.rootStateForSheet.presence(),
 				onDismiss: {
