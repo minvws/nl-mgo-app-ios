d8215c14b071c955fb2df432c57d4cf29e44918b
Rool Paap <rool@rool.nl>
2024-10-24 16:43:17 +0200

    chore: cleanup obsolete methods

--- Sources/MGO/Features/Splash/SnapshotView.swift
+++ Sources/MGO/Features/Splash/SnapshotView.swift
@@ -14,46 +14,6 @@ struct SnapshotView: View {
 	/// The Theme
 	@Environment(\.theme) var theme
 	
-	@State private var rijkslintTopOffset: CGFloat = 0
-	@State private var spinnerBottomPadding: CGFloat = 0
-	
-	private struct ViewTraits {
-		enum DynamicIsland {
-			static let height: CGFloat = 59
-			static let offset: CGFloat = 11
-		}
-		enum Notch {
-			static let height: CGFloat = 47
-			static let offset: CGFloat = 16
-		}
-		enum Title {
-			static let topOffset: CGFloat = 64
-		}
-		enum Spinner {
-			static let bottomOffset: CGFloat = 75
-		}
-		enum Rijkslint {
-			static let height: CGFloat = 100
-			static let width: CGFloat = 50
-		}
-	}
-	
-	/// Calculate the offset for the rijkslint so it stays just below the notch or dynamic island
-	/// - Parameter safeAreaHeight: the height of the safe area
-	func recalculateOffset(_ safeAreaInsets: EdgeInsets) {
-		if safeAreaInsets.top >= ViewTraits.DynamicIsland.height {
-			rijkslintTopOffset = safeAreaInsets.top - ViewTraits.DynamicIsland.offset
-		} else if safeAreaInsets.top >= ViewTraits.Notch.height {
-			rijkslintTopOffset = safeAreaInsets.top - ViewTraits.Notch.offset
-		} else {
-			rijkslintTopOffset = 0
-		}
-	}
-	
-	private func recalculateBottomPadding(_ safeAreaInsets: EdgeInsets) {
-		spinnerBottomPadding = 70 - safeAreaInsets.bottom
-	}
-	
 	var body: some View {
 		GeometryReader { geometry in
 			ZStack {
