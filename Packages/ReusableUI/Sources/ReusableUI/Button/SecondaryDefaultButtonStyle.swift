3975cf05986752fee75160c235bb68de450d90b2
Rool Paap <rool@rool.nl>
2024-10-31 09:29:13 +0100

    style: increase corner radius on CTA buttons from 8 to 12 px

--- Packages/ReusableUI/Sources/ReusableUI/Button/SecondaryDefaultButtonStyle.swift
+++ Packages/ReusableUI/Sources/ReusableUI/Button/SecondaryDefaultButtonStyle.swift
@@ -20,7 +20,7 @@ struct SecondaryDefaultButtonStyle: ButtonStyle {
 			static let insets = EdgeInsets( top: 16, leading: 24, bottom: 16, trailing: 24)
 		}
 		enum Button {
-			static let cornerRadius: CGFloat = 8
+			static let cornerRadius: CGFloat = 12
 			static let minimumHeight: CGFloat = 48
 		}
 	}
--- Packages/ReusableUI/Sources/ReusableUI/Button/SecondaryNegativeButtonStyle.swift
+++ Packages/ReusableUI/Sources/ReusableUI/Button/SecondaryNegativeButtonStyle.swift
@@ -20,7 +20,7 @@ struct SecondaryNegativeButtonStyle: ButtonStyle {
 			static let insets = EdgeInsets( top: 16, leading: 24, bottom: 16, trailing: 24)
 		}
 		enum Button {
-			static let cornerRadius: CGFloat = 8
+			static let cornerRadius: CGFloat = 12
 			static let minimumHeight: CGFloat = 48
 		}
 	}
