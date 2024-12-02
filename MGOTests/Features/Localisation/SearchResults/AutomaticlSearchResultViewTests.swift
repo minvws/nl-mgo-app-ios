ff9eb6d4d1168f0ad7c040067932f75d5016594f
Rool Paap <rool@rool.nl>
2024-12-02 14:09:37 +0100

    test: added tests for AutomaticSearchResultsView

---
+++ MGOTests/Features/Localisation/SearchResults/AutomaticlSearchResultViewTests.swift
@@ -0,0 +1,171 @@
+/*
+ *  Copyright (c) 2024 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
+ *  Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
+ *
+ *  SPDX-License-Identifier: EUPL-1.2
+ */
+
+import MGOTest
+@testable import MGO
+import MGOFoundation
+import MGOUI
+
+final class AutomaticSearchResultViewTests: XCTestCase {
+	
+	private var coordinatorSpy: AppCoordinatorSpy!
+	private var localisationServiceClientSpy: LocalisationServiceClientSpy!
+	private var servicesSpies: ServicesSpies!
+	private var viewModel: AutomaticSearchResultsViewModel!
+	private var sut: AutomaticSearchResultsView!
+
+	override func setUpWithError() throws {
+		
+		try super.setUpWithError()
+		servicesSpies = setupServicesSpies()
+		coordinatorSpy = AppCoordinatorSpy()
+		let serverUrl = try XCTUnwrap(URL(string: "https://example.com"))
+		localisationServiceClientSpy = LocalisationServiceClientSpy(serverUrl: serverUrl, username: nil, password: nil)
+	}
+	
+	private func createSut(preselectAllOrganizations: Bool = true) {
+		
+		viewModel = AutomaticSearchResultsViewModel(
+			coordinator: coordinatorSpy,
+			localisationServiceClient: localisationServiceClientSpy,
+			preselectAllOrganizations: preselectAllOrganizations
+		)
+		
+		sut = AutomaticSearchResultsView(viewModel: self.viewModel)
+	}
+	
+	func test_loading() {
+		
+		// Given
+		createSut()
+		viewModel.state = .loading
+		
+		// When
+		let content = NavigationView { sut }
+		
+		// Then
+		takeSnapShots(content: content)
+	}
+	
+	func test_failure() {
+		
+		// Given
+		createSut()
+		let error = NSError(domain: "SearchResultViewModelTests", code: 404)
+		viewModel.state = .failure(error)
+		
+		// When
+		let content = NavigationView { sut }
+		
+		// Then
+		takeSnapShots(content: content)
+	}
+	
+	func test_failure_action() throws {
+		
+		// Given
+		createSut()
+		let error = NSError(domain: "SearchResultViewModelTests", code: 404)
+		viewModel.state = .failure(error)
+		
+		// When
+		let view = try sut.inspect().find(viewWithAccessibilityIdentifier: "action_button")
+		try view.view(CallToActionButton.self).find(button: "common.try_again").tap()
+		
+		// Then
+		viewModel.state = .failure(error)
+		expect(self.localisationServiceClientSpy.invokedSearchHealthcareOrganizations).toEventually(beTrue(), timeout: .seconds(5))
+	}
+	
+	func test_list_lightPortrait() {
+		
+		// Given
+		createSut()
+		let list: [OrganizationSearchResultSet] = [
+			((Generator.healthcareOrganization("1"), OrganizationSearchResultCardState.automatic(isSelected: true))),
+			((Generator.healthcareOrganization("2"), OrganizationSearchResultCardState.notParticipating)),
+			((Generator.healthcareOrganization("3"), OrganizationSearchResultCardState.automatic(isSelected: false))),
+			((Generator.healthcareOrganization("4", city: "", address: "", postalCode: ""), OrganizationSearchResultCardState.selected))
+		]
+		viewModel.state = .success(list)
+		
+		// When
+		let content = NavigationView { sut }
+		
+		// Then
+		assertSnapshot(
+			of: UIHostingController(rootView: content.colorScheme(.light)),
+			as: .image(on: .iPhone16Pro(.portrait), precision: 1.0)
+		)
+	}
+	
+	func test_list_darkPortrait() {
+		
+		// Given
+		createSut()
+		let list: [OrganizationSearchResultSet] = [
+			((Generator.healthcareOrganization("1"), OrganizationSearchResultCardState.automatic(isSelected: true))),
+			((Generator.healthcareOrganization("2"), OrganizationSearchResultCardState.notParticipating)),
+			((Generator.healthcareOrganization("3"), OrganizationSearchResultCardState.automatic(isSelected: false))),
+			((Generator.healthcareOrganization("4", city: "", address: "", postalCode: ""), OrganizationSearchResultCardState.selected))
+		]
+		viewModel.state = .success(list)
+		
+		// When
+		let content = NavigationView { sut }
+		
+		// Then
+		assertSnapshot(
+			of: UIHostingController(rootView: content.colorScheme(.dark)),
+			as: .image(on: .iPhone16Pro(.portrait), precision: 1.0)
+		)
+	}
+	
+	func test_list_lightLandscape() {
+		
+		// Given
+		createSut()
+	let list: [OrganizationSearchResultSet] = [
+		((Generator.healthcareOrganization("1"), OrganizationSearchResultCardState.automatic(isSelected: true))),
+		((Generator.healthcareOrganization("2"), OrganizationSearchResultCardState.notParticipating)),
+		((Generator.healthcareOrganization("3"), OrganizationSearchResultCardState.automatic(isSelected: false))),
+		((Generator.healthcareOrganization("4", city: "", address: "", postalCode: ""), OrganizationSearchResultCardState.selected))
+	]
+		viewModel.state = .success(list)
+		
+		// When
+		let content = NavigationView { sut }
+		
+		// Then
+		assertSnapshot(
+			of: UIHostingController(rootView: content.colorScheme(.light)),
+			as: .image(on: .iPhone16Pro(.landscape), precision: 1.0)
+		)
+	}
+	
+	func test_list_darkLandscape() {
+		
+		// Given
+		createSut()
+		let list: [OrganizationSearchResultSet] = [
+			((Generator.healthcareOrganization("1"), OrganizationSearchResultCardState.automatic(isSelected: true))),
+			((Generator.healthcareOrganization("2"), OrganizationSearchResultCardState.notParticipating)),
+			((Generator.healthcareOrganization("3"), OrganizationSearchResultCardState.automatic(isSelected: false))),
+			((Generator.healthcareOrganization("4", city: "", address: "", postalCode: ""), OrganizationSearchResultCardState.selected))
+		]
+		viewModel.state = .success(list)
+		
+		// When
+		let content = NavigationView { sut }
+		
+		// Then
+		assertSnapshot(
+			of: UIHostingController(rootView: content.colorScheme(.dark)),
+			as: .image(on: .iPhone16Pro(.landscape), precision: 1.0)
+		)
+	}
+}
