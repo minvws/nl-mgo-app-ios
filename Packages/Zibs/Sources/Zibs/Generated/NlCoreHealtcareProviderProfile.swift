3064f88530db301a93320c6ab9d347b5fa9efcf1
Rool Paap <rool@rool.nl>
2024-12-24 09:32:05 +0100

    ci: Zib import

---
+++ Packages/Zibs/Sources/Zibs/Generated/NlCoreHealtcareProviderProfile.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+public enum NlCoreHealtcareProviderProfile: String, Codable, Hashable, Sendable {
+    case httpNictizNlFhirStructureDefinitionNlCoreHealthcareProvider = "http://nictiz.nl/fhir/StructureDefinition/nl-core-HealthcareProvider"
+}
---
+++ Packages/Zibs/Sources/Zibs/Generated/NlCorePharmaceuticalProductR4Profile.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+public enum NlCorePharmaceuticalProductR4Profile: String, Codable, Hashable, Sendable {
+    case httpNictizNlFhirStructureDefinitionNlCorePharmaceuticalProduct = "http://nictiz.nl/fhir/StructureDefinition/nl-core-PharmaceuticalProduct"
+}
