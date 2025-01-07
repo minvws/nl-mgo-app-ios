4d2d3184f156116fea57b1a29e5700f3ede04655
Rool Paap <rool@rool.nl>
2025-01-07 09:03:20 +0100

    refactor: Split parser into separate files

---
+++ Packages/FHIRParser/Sources/FHIRParser/ParseError.swift
@@ -0,0 +1,30 @@
+/*
+ *  Copyright (c) 2025 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
+ *  Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
+ *
+ *  SPDX-License-Identifier: EUPL-1.2
+ */
+
+import Foundation
+
+/// the FHIR parse errors
+public enum FHIRParserError: Error {
+	
+	// The input could not be converted
+	case invalidInput
+	
+	// This method is not available in the JS parser
+	case invalidMethod
+	
+	// This namespace is not available in the JS parser
+	case invalidNameSpace
+	
+	// Failed to initiate a JS Context
+	case noJSContext
+	
+	// There was no output
+	case noResult
+	
+	// The parser was not found at its location
+	case parserNotFound
+}
