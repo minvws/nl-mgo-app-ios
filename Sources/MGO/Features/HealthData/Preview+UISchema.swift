3003f56755ec55158974bfda23a70f73589c7220
Rool Paap <rool@rool.nl>
2025-01-09 14:10:45 +0100

    ci: reduce duplicate preview content

---
+++ Sources/MGO/Features/HealthData/Preview+UISchema.swift
@@ -0,0 +1,89 @@
+/*
+ *  Copyright (c) 2024 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
+ *  Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
+ *
+ *  SPDX-License-Identifier: EUPL-1.2
+ */
+
+import MGOFoundation
+import Zibs
+
+extension PreviewContent {
+
+	static let uiSchema = UISchema(
+		children: [
+			// Schema Group 1
+			UISchemaGroup(
+				children: [
+					UIElement(
+						display: UIElementDisplay.string("single value"),
+						label: "label single value",
+						type: .singleValue,
+						reference: nil,
+						url: nil
+					),
+					UIElement(
+						display: nil,
+						label: "label reference value",
+						type: .referenceValue,
+						reference: "reference",
+						url: "Ref/123"
+					),
+					UIElement(
+						display: nil,
+						label: "label reference link",
+						type: .referenceLink,
+						reference: "reference",
+						url: "Ref/123"
+					),
+					UIElement(
+						display: nil,
+						label: "label download link",
+						type: .downloadLink,
+						reference: nil,
+						url: "https://www.apple.com"
+					)
+				],
+				label: "Section Header first group"
+			),
+			// Schema Group 2
+			UISchemaGroup(
+				children: [
+					// Unknown
+					UIElement(
+						display: nil,
+						label: "label single value nil",
+						type: .singleValue,
+						reference: nil,
+						url: nil
+					),
+					UIElement(
+						display: UIElementDisplay.unionArray([
+							DisplayElement.stringArray(["one", "two"]),
+							DisplayElement.stringArray(["three", "four"])
+						]),
+						label: "label multiple group value",
+						type: .multipleGroupedValues,
+						reference: nil,
+						url: nil
+					),
+					UIElement(
+						display: UIElementDisplay.unionArray([DisplayElement.stringArray(["one", "two"])]),
+						label: "label multiple value",
+						type: .multipleValues,
+						reference: nil,
+						url: nil
+					),
+					UIElement(
+						display: UIElementDisplay.unionArray([DisplayElement.string("one")]),
+						label: "label union value",
+						type: .multipleValues,
+						reference: nil,
+						url: nil
+					)
+				],
+				label: "Section Header second group")
+		],
+		label: "UI Schema"
+	)
+}
--- Sources/MGO/Features/HealthData/UISchemaView.swift
+++ Sources/MGO/Features/HealthData/UISchemaView.swift
@@ -260,77 +260,7 @@ struct UISchemaView: View {
 
 #Preview {
 	UISchemaView(
-		schema:
-			UISchema(
-				children: [
-					// Schema Group 1
-					UISchemaGroup(
-						children: [
-							UIElement(
-								display: UIElementDisplay.string("single value"),
-								label: "label single value",
-								type: .singleValue,
-								reference: nil,
-								url: nil
-							),
-							
-							UIElement(
-								display: UIElementDisplay.string("reference value"),
-								label: "label reference",
-								type: .referenceValue,
-								reference: "reference",
-								url: nil
-							),
-							UIElement(
-								display: nil,
-								label: "label download link",
-								type: .downloadLink,
-								reference: nil,
-								url: "https://www.apple.com"
-							)
-						],
-						label: "Section Header first group"),
-					
-					// Schema Group 2
-					UISchemaGroup(
-						children: [
-							// Unknown
-							UIElement(
-								display: nil,
-								label: "label single value nil",
-								type: .singleValue,
-								reference: nil,
-								url: nil
-							),
-							UIElement(
-								display: UIElementDisplay.unionArray([
-									DisplayElement.stringArray(["one", "two"]),
-									DisplayElement.stringArray(["three", "four"])
-								]),
-								label: "label multiple group value",
-								type: .multipleGroupedValues,
-								reference: nil,
-								url: nil
-							),
-							UIElement(
-								display: UIElementDisplay.unionArray([DisplayElement.stringArray(["one", "two"])]),
-								label: "label multiple value",
-								type: .multipleValues,
-								reference: nil,
-								url: nil
-							),
-							UIElement(
-								display: UIElementDisplay.unionArray([DisplayElement.string("one")]),
-								label: "label union value",
-								type: .multipleValues,
-								reference: nil,
-								url: nil
-							)
-						],
-						label: "Section Header second group")
-				],
-				label: "UI Schema"
-			),
+		schema: PreviewContent.uiSchema,
 		healthcareOrganization: PreviewContent.healthcareOrganization,
 		resolvedReferences: ["reference": true]
 	)
