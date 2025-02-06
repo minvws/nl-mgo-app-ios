30c8aabc33bfcd70dba615b9140c522dee7dfc54
Rool Paap <rool@rool.nl>
2025-02-06 15:41:39 +0100

    test: Removed obsolete test

--- MGOTests/Features/HealthData/HealthDataViewModelTests.swift
+++ MGOTests/Features/HealthData/HealthDataViewModelTests.swift
@@ -170,24 +170,4 @@ final class HealthDataViewModelTests: XCTestCase {
 		expect(params.params["backButtonTitle"] as? String) == "common.previous"
 		expect((params.params["uiSchema"] as? UISchema)?.label) == schema.label
 	}
-	
-	func test_resolveReferenceValue_labelIsEmpty_shouldCallCoordinator() throws {
-		
-		// Given
-		let schema = UISchema(children: [], label: nil)
-		self.referenceResolverSpy.stubbedResolveResult = (Data(), schema)
-		setupSut()
-		
-		// When
-		sut.reduce(.reference("test_resolveReference"))
-		
-		// Then
-		expect(self.coordinatorSpy.invokedHandle) == true
-		
-		let params = try XCTUnwrap(self.coordinatorSpy.invokedHandleParameters?.0)
-		expect(params.identifier) == Coordination.Action.showHealthData.identifier
-		expect(params.params["resource"] as? MgoResource) == Data()
-		expect(params.params["backButtonTitle"] as? String) == "common.previous"
-		expect((params.params["uiSchema"] as? UISchema)?.label) == nil
-	}
 }
