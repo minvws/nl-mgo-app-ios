58d89684aa43c3f362752d5036b0c2b13dd9b514
Rool Paap <rool@rool.nl>
2025-02-25 08:18:13 +0100

    ci: updated MGOUI package

---
+++ Packages/MGOUI/Sources/MGOUI/Helpers.swift
@@ -0,0 +1,19 @@
+/*
+ *  Copyright (c) 2025 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
+ *  Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
+ *
+ *  SPDX-License-Identifier: EUPL-1.2
+ */
+
+import Foundation
+
+public var isiPhoneSE: Bool {
+	Device.current == .iPhoneSE || Device.current == .simulator(.iPhoneSE)
+}
+
+public var isIOS15: Bool {
+	if #unavailable(iOS 16) {
+		return true
+	}
+	return false
+}
