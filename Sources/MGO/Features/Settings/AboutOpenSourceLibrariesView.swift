f2e4b13ad1e1e84825a71fc29263beef474b1ae0
Rool Paap <rool@rool.nl>
2025-03-26 16:06:54 +0100

    refactor: Create Library object

--- Sources/MGO/Features/Settings/AboutOpenSourceLibrariesView.swift
+++ Sources/MGO/Features/Settings/AboutOpenSourceLibrariesView.swift
@@ -9,9 +9,17 @@ import MGOFoundation
 import MGOUI
 
 class AboutOpenSourceLibrariesViewModel: ObservableObject {
+
+	struct Library: Identifiable {
+		let id = UUID()
+		var name: String
+		var urlString: String
+	}
 	
 	/// The app coordinator for routing
 	weak var coordinator: (any Coordinator)?
+	
+	@Published var libraries: [AboutOpenSourceLibrariesViewModel.Library] = []
 	
 	/// A list of all the actions this viewModel can handle
 	enum Action {
@@ -23,6 +31,73 @@ class AboutOpenSourceLibrariesViewModel: ObservableObject {
 	/// - Parameter coordinator: the app coordinator
 	init(coordinator: (any Coordinator)? = nil) {
 		self.coordinator = coordinator
+		
+		libraries = [
+			Library(
+				name: "DeviceKit (MIT)",
+				urlString: "https://github.com/devicekit/DeviceKit?tab=MIT-1-ov-file#readme" // NOSONAR
+			),
+			Library(
+				name: "Figlet (Apache 2.0)",
+				urlString: "https://github.com/apple/example-package-figlet?tab=Apache-2.0-1-ov-file#readme" // NOSONAR
+			),
+			Library(name:
+				"iOS Security Suite (BSD 2)",
+				urlString: "https://github.com/securing/IOSSecuritySuite/tree/1.9.11?tab=License-1-ov-file" // NOSONAR
+			),
+			Library(name:
+				"Nimble (Apache 2.0)",
+				urlString: "https://github.com/Quick/Nimble?tab=Apache-2.0-1-ov-file#readme" // NOSONAR
+			),
+			Library(name:
+				"OHHTTPStubs (MIT)",
+				urlString: "https://github.com/AliSoftware/OHHTTPStubs?tab=MIT-1-ov-file#readme" // NOSONAR
+			),
+			Library(name:
+				"RDO Modules - Logging (EUPL 1.2)",
+				urlString: "https://github.com/minvws/nl-rdo-app-ios-modules?tab=EUPL-1.2-1-ov-file" // NOSONAR
+			),
+			Library(name:
+				"📸 SnapshotTesting (MIT)",
+				urlString: "https://github.com/pointfreeco/swift-snapshot-testing?tab=MIT-1-ov-file#readme" // NOSONAR
+			),
+			Library(name:
+				"Swift Argument Parser (Apache 2.0)",
+				urlString: "https://github.com/apple/swift-argument-parser?tab=Apache-2.0-1-ov-file#readme" // NOSONAR
+			),
+			Library(name:
+				"Swift HTTP Types (Apache 2.0)",
+				urlString: "https://github.com/apple/swift-http-types?tab=Apache-2.0-1-ov-file#readme" // NOSONAR
+			),
+			Library(name:
+				"Swift OpenAPI Generator (Apache 2.0)",
+				urlString: "https://github.com/apple/swift-openapi-generator#Apache-2.0-1-ov-file" // NOSONAR
+			),
+			Library(name:
+				"Swift OpenAPI Generator Runtime (Apache 2.0)",
+				urlString: "https://github.com/apple/swift-openapi-runtime?tab=Apache-2.0-1-ov-file#Apache-2.0-1-ov-file" // NOSONAR
+			),
+			Library(name:
+				"SwiftSoup (MIT)",
+				urlString: "https://github.com/scinfu/SwiftSoup?tab=License-1-ov-file#readme" // NOSONAR
+			),
+			Library(name:
+				"SwiftUI Introspect (MIT)",
+				urlString: "https://github.com/siteline/SwiftUI-Introspect?tab=MIT-1-ov-file#readme" // NOSONAR
+			),
+			Library(name:
+				"SwiftUI NavigationStack Backport (MIT)",
+				urlString: "https://github.com/lm/navigation-stack-backport?tab=MIT-1-ov-file#readme" // NOSONAR
+			),
+			Library(name:
+				"URLSession Transport for Swift OpenAPI Generator (Apache 2.0)",
+				urlString: "https://github.com/apple/swift-openapi-urlsession?tab=Apache-2.0-1-ov-file#readme" // NOSONAR
+			),
+			Library(name:
+				"ViewInspector 🕵️‍♂️ for SwiftUI (MIT)",
+				urlString: "https://github.com/nalexn/ViewInspector?tab=MIT-1-ov-file#readme" // NOSONAR
+			)
+		]
 	}
 	
 	/// Handle any action
@@ -71,87 +146,7 @@ struct AboutOpenSourceLibrariesView: View {
 		
 		List {
 			Section {
-				
-				row(
-					"DeviceKit (MIT)",
-					urlString: "https://github.com/devicekit/DeviceKit?tab=MIT-1-ov-file#readme"
-				)
-				
-				row(
-					"Figlet (Apache 2.0)",
-					urlString: "https://github.com/apple/example-package-figlet?tab=Apache-2.0-1-ov-file#readme"
-				)
-				
-				row(
-					"iOS Security Suite (BSD 2)",
-					urlString: "https://github.com/securing/IOSSecuritySuite/tree/1.9.11?tab=License-1-ov-file"
-				)
-				
-				row(
-					"Nimble (Apache 2.0)",
-					urlString: "https://github.com/Quick/Nimble?tab=Apache-2.0-1-ov-file#readme"
-				)
-				
-				row(
-					"OHHTTPStubs (MIT)",
-					urlString: "https://github.com/AliSoftware/OHHTTPStubs?tab=MIT-1-ov-file#readme"
-				)
-				
-				row(
-					"RDO Modules - Logging (EUPL 1.2)",
-					urlString: "https://github.com/minvws/nl-rdo-app-ios-modules?tab=EUPL-1.2-1-ov-file"
-				)
-				
-				row(
-					"📸 SnapshotTesting (MIT)",
-					urlString: "https://github.com/pointfreeco/swift-snapshot-testing?tab=MIT-1-ov-file#readme"
-				)
-				
-				row(
-					"Swift Argument Parser (Apache 2.0)",
-					urlString: "https://github.com/apple/swift-argument-parser?tab=Apache-2.0-1-ov-file#readme"
-				)
-				
-				row(
-					"Swift HTTP Types (Apache 2.0)",
-					urlString: "https://github.com/apple/swift-http-types?tab=Apache-2.0-1-ov-file#readme"
-				)
-				
-				row(
-					"Swift OpenAPI Generator (Apache 2.0)",
-					urlString: "https://github.com/apple/swift-openapi-generator#Apache-2.0-1-ov-file"
-				)
-				
-				row(
-					"Swift OpenAPI Generator Runtime (Apache 2.0)",
-					urlString: "https://github.com/apple/swift-openapi-runtime?tab=Apache-2.0-1-ov-file#Apache-2.0-1-ov-file"
-				)
-				
-				row(
-					"SwiftSoup (MIT)",
-					urlString: "https://github.com/scinfu/SwiftSoup?tab=License-1-ov-file#readme"
-				)
-				
-				row(
-					"SwiftUI Introspect (MIT)",
-					urlString: "https://github.com/siteline/SwiftUI-Introspect?tab=MIT-1-ov-file#readme"
-				)
-				
-				row(
-					"SwiftUI NavigationStack Backport (MIT)",
-					urlString: "https://github.com/lm/navigation-stack-backport?tab=MIT-1-ov-file#readme"
-				)
-				
-				row(
-					"URLSession Transport for Swift OpenAPI Generator (Apache 2.0)",
-					urlString: "https://github.com/apple/swift-openapi-urlsession?tab=Apache-2.0-1-ov-file#readme"
-				)
-				
-				row(
-					"ViewInspector 🕵️‍♂️ for SwiftUI (MIT)",
-					urlString: "https://github.com/nalexn/ViewInspector?tab=MIT-1-ov-file#readme"
-				)
-				
+				list()
 			} header: {
 				Text("settings.about_this_app.open_source.subheading")
 					.rijksoverheidStyle(font: .regular, style: .body)
@@ -171,6 +166,15 @@ struct AboutOpenSourceLibrariesView: View {
 		.navigationBarTitleDisplayMode(.inline)
 		.background(theme.backgroundPrimary.ignoresSafeArea())
 		.layoutForIPad()
+	}
+	
+	/// Get a list of libraries
+	/// - Returns: a list of libraries
+	@ViewBuilder private func list() -> some View {
+		
+		ForEach(viewModel.libraries, id: \.id) { library in
+			row(library.name, urlString: library.urlString)
+		}
 	}
 	
 	/// Build a row for the open source libraries used
