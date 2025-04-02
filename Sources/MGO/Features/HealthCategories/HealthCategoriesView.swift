5d5c7149f6b67c8ee97393dec756d5ee9d7f0c3f
Rool Paap <rool@rool.nl>
2025-04-02 13:08:43 +0200

    ci: improve code coverage

--- Sources/MGO/Features/HealthCategories/HealthCategoriesView.swift
+++ Sources/MGO/Features/HealthCategories/HealthCategoriesView.swift
@@ -27,6 +27,7 @@ struct HealthCategoriesViewState {
 	var showRemoveHealthcareProvider: Bool
 	var healthCategories: [CategoryButton]
 	var backButtonTitle: LocalizedStringKey?
+	var belowIOS18: Bool
 	
 	mutating func updateCategoryState(id: Int, state: CategoryButtonState) {
 		withAnimation {
@@ -130,7 +131,8 @@ class HealthCategoriesViewModel: ObservableObject {
 				CategoryButton(category: .mentalWellbeing, title: "hc_mental.heading", box: disabledForDemoBox),
 				CategoryButton(category: .lifestyle, title: "hc_lifestyle.heading", box: disabledForDemoBox)
 			],
-			backButtonTitle: backbuttonTitle
+			backButtonTitle: backbuttonTitle,
+			belowIOS18: belowIOS18
 		)
 		
 		registerObservers()
@@ -360,10 +362,9 @@ struct HealthCategoriesView: View {
 		.when(viewModel.state.canTitleCollapse) { view in
 			view.navigationTitle(viewModel.state.heading)
 		}
-		.when(belowIOS18 && !viewModel.state.canTitleCollapse) { view in
+		.when(viewModel.state.belowIOS18 && !viewModel.state.canTitleCollapse) { view in
 			view
 				.navigationBarTitleDisplayMode(.inline)
-				.logInfo("inline")
 		}
 		.navigationBarHidden(false)
 		.background(theme.backgroundPrimary.ignoresSafeArea())
