2efbd5a54ec81c2f852de2b3ca245df3e7b1c15e
Rool Paap <rool@rool.nl>
2025-05-14 10:46:51 +0200

    test: added testcase

---
+++ Packages/ReusableUI/Tests/ReusableUITests/ScreenshotAlertModifierTests.swift
@@ -0,0 +1,25 @@
+/*
+ *  Copyright (c) 2025 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
+ *  Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
+ *
+ *  SPDX-License-Identifier: EUPL-1.2
+ */
+
+@testable import ReusableUI
+import SwiftUI
+import MGOTest
+
+final class ScreenshotAlertModifierTests: XCTestCase {
+	
+	func test_alert() throws {
+		
+		// Given
+		let sut = Text("Screenshot Alert").screenshotAlert()
+		
+		// When
+		NotificationCenter.default.post(name: UIApplication.userDidTakeScreenshotNotification, object: nil)
+		
+		// Then
+		takeSnapShots(content: sut)
+	}
+}
