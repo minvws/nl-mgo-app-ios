69402336cd132b3b9eb8c235ced9427f4dcb89fe
Rool Paap <rool@rool.nl>
2025-06-26 16:00:07 +0200

    feat: Combine subcategories for demo, add GP medication request

--- Sources/MGO/Features/HealthCategories/HealthCategories.swift
+++ Sources/MGO/Features/HealthCategories/HealthCategories.swift
@@ -168,11 +168,14 @@ struct HealthCategories {
 				
 				// Labresults
 				case ZibLaboratoryTestResultObservationProfile.httpNictizNlFhirStructureDefinitionZibLaboratoryTestResultObservation.rawValue:
-					"zib_laboratory_test_result_observation.heading"
+					
+					Current.featureFlagManager.isDemo ? "zib_laboratory_demo.heading" : "zib_laboratory_test_result_observation.heading"
+				
 				case ZibLaboratoryTestResultSpecimenProfile.httpNictizNlFhirStructureDefinitionZibLaboratoryTestResultSpecimen.rawValue:
 					"zib_laboratory_test_result_specimen.heading"
 				case GpLaboratoryResultProfile.httpNictizNlFhirStructureDefinitionGpLaboratoryResult.rawValue:
-					"gp_laboratory_result.heading"
+				
+					Current.featureFlagManager.isDemo ? "zib_laboratory_demo.heading" : "gp_laboratory_result.heading"
 				
 				// Allergies
 				case ZibAllergyIntoleranceProfile.httpNictizNlFhirStructureDefinitionZibAllergyIntolerance.rawValue:
@@ -351,7 +354,8 @@ struct HealthCategories {
 		private var demoServices: [DVP.Endpoint] {
 			switch self {
 				case .medication: return [
-					DVP.CommonClinicalDataset.medicationUse
+					DVP.CommonClinicalDataset.medicationUse,
+					DVP.GeneralPractitioner.currentMedication()
 				]
 					
 				case .labResults: return [
