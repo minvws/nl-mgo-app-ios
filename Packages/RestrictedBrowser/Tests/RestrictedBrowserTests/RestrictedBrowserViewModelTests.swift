6fa19898d4cda531bf02a418744a752d74e9581b
Rool Paap <rool@rool.nl>
2025-07-03 10:12:28 +0200

    test: added webview delegate tests

--- Packages/RestrictedBrowser/Tests/RestrictedBrowserTests/RestrictedBrowserViewModelTests.swift
+++ Packages/RestrictedBrowser/Tests/RestrictedBrowserTests/RestrictedBrowserViewModelTests.swift
@@ -59,4 +59,129 @@ final class RestrictedBrowserViewModelTests: XCTestCase {
 		// Then
 		expect(testAction.receivedPolicy) == .cancel
 	}
+	
+	func test_didReceive_authenticationMode_serverTrust() throws {
+		
+		// Given
+		let challenge = challenge(authenticationMethod: NSURLAuthenticationMethodServerTrust)
+		let urlOpenerSpy = URLOpenerSpy()
+		urlOpenerSpy.stubbedCanOpenURLResult = true
+		let url = try XCTUnwrap(URL(string: "https://support.apple.com"))
+		let browser = RestrictedBrowser(allowedDomains: ["localhost"], urlOpener: urlOpenerSpy)
+		let sut = RestrictedBrowserViewModel(url: url, browser: browser)
+		let completionHandler = MockWebViewDidReceiveCompletionHandler()
+		
+		// When
+		sut.webView(
+			WKWebView(),
+			didReceive: challenge,
+			completionHandler: completionHandler.completionHandler
+		)
+		
+		// Then
+		expect(completionHandler.disposition) == .performDefaultHandling
+		expect(completionHandler.credential) == nil
+	}
+	
+	func test_didReceive_authenticationMode_default_withoutCredentials() throws {
+		
+		// Given
+		let challenge = challenge(authenticationMethod: NSURLAuthenticationMethodDefault)
+		let urlOpenerSpy = URLOpenerSpy()
+		urlOpenerSpy.stubbedCanOpenURLResult = true
+		let url = try XCTUnwrap(URL(string: "https://support.apple.com"))
+		let browser = RestrictedBrowser(allowedDomains: ["localhost"], urlOpener: urlOpenerSpy)
+		let sut = RestrictedBrowserViewModel(url: url, browser: browser)
+		let completionHandler = MockWebViewDidReceiveCompletionHandler()
+		
+		// When
+		sut.webView(
+			WKWebView(),
+			didReceive: challenge,
+			completionHandler: completionHandler.completionHandler
+		)
+		
+		// Then
+		expect(completionHandler.disposition) == .cancelAuthenticationChallenge
+		expect(completionHandler.credential) == nil
+	}
+	
+	func test_didReceive_authenticationMode_default() throws {
+		
+		// Given
+		let challenge = challenge(authenticationMethod: NSURLAuthenticationMethodDefault)
+		let urlOpenerSpy = URLOpenerSpy()
+		urlOpenerSpy.stubbedCanOpenURLResult = true
+		let url = try XCTUnwrap(URL(string: "https://support.apple.com"))
+		let browser = RestrictedBrowser(allowedDomains: ["localhost"], urlOpener: urlOpenerSpy)
+		let sut = RestrictedBrowserViewModel(
+			url: url,
+			browser: browser,
+			authUsername: "username",
+			authPassword: "password"
+		)
+		let completionHandler = MockWebViewDidReceiveCompletionHandler()
+		
+		// When
+		sut.webView(
+			WKWebView(),
+			didReceive: challenge,
+			completionHandler: completionHandler.completionHandler
+		)
+		
+		// Then
+		expect(completionHandler.disposition) == .useCredential
+		expect(completionHandler.credential) == URLCredential(user: "username", password: "password", persistence: .forSession)
+	}
+	
+	func test_didReceive_authenticationMode_other() throws {
+		
+		// Given
+		let challenge = challenge(authenticationMethod: NSURLAuthenticationMethodNegotiate)
+		let urlOpenerSpy = URLOpenerSpy()
+		urlOpenerSpy.stubbedCanOpenURLResult = true
+		let url = try XCTUnwrap(URL(string: "https://support.apple.com"))
+		let browser = RestrictedBrowser(allowedDomains: ["localhost"], urlOpener: urlOpenerSpy)
+		let sut = RestrictedBrowserViewModel(
+			url: url,
+			browser: browser,
+			authUsername: "username",
+			authPassword: "password"
+		)
+		let completionHandler = MockWebViewDidReceiveCompletionHandler()
+		
+		// When
+		sut.webView(
+			WKWebView(),
+			didReceive: challenge,
+			completionHandler: completionHandler.completionHandler
+		)
+		
+		// Then
+		expect(completionHandler.disposition) == .cancelAuthenticationChallenge
+		expect(completionHandler.credential) == nil
+	}
+	
+	// MARK: - helper
+	
+	/// Create a URLAuthenticationChallenge
+	/// - Parameter authenticationMethod: the authentication method to use
+	/// - Returns: URLAuthenticationChallenge
+	private func challenge(authenticationMethod: String) -> URLAuthenticationChallenge {
+		
+		return URLAuthenticationChallenge(
+			protectionSpace: URLProtectionSpace(
+				host: "localhost",
+				port: 443,
+				protocol: "https",
+				realm: "Mijn Gezondheidsoverzicht",
+				authenticationMethod: authenticationMethod
+			),
+			proposedCredential: nil,
+			previousFailureCount: 0,
+			failureResponse: nil,
+			error: nil,
+			sender: URLAuthenticationChallengeSenderSpy()
+		)
+	}
 }
