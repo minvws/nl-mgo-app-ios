f7227f864322b8325b3dc8dfb2f0486c799453f3
Rool Paap <rool@rool.nl>
2025-07-10 12:52:23 +0200

    chore: more cleanup

--- Sources/MGO/Features/Settings/Coordinator/SettingsCoordinator.swift
+++ Sources/MGO/Features/Settings/Coordinator/SettingsCoordinator.swift
@@ -14,7 +14,6 @@ extension Coordination.Action {
 	static let openUrl = Coordination.Action(identifier: "openUrl")
 	static let showAboutTheApp = Coordination.Action(identifier: "showAboutTheApp")
 	static let showAccessibility = Coordination.Action(identifier: "showAccessibility")
-	static let showAccessibilityMoreInformation = Coordination.Action(identifier: "showAccessibilityMoreInformation")
 	static let showAdvancedSettings = Coordination.Action(identifier: "showAdvancedSettings")
 	static let showDisplaySettings = Coordination.Action(identifier: "showDisplaySettings")
 	static let showOpenSourceLibraries = Coordination.Action(identifier: "showOpenSourceLibraries")
