b51a8e5b7a21abe8e6abd4ac4782999f24b19e5a
Rool Paap <rool@rool.nl>
2025-07-14 14:08:46 +0200

    refactor: updated Localisation Service package

--- Packages/LocalisationService/Package.swift
+++ Packages/LocalisationService/Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
 	name: "LocalisationService",
-	platforms: [.iOS(.v15)],
+	platforms: [.macOS(.v10_15), .iOS(.v15)],
 	products: [
 		.library(
 			name: "LocalisationService",
