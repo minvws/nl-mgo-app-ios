7965c5047daf469a79b6854dce3625ebd37354be
Rool Paap <rool@rool.nl>
2025-08-04 10:55:11 +0200

    refactor: renamed viewState method

--- Sources/MGO/Features/Dashboard/DashboardCoordinator.swift
+++ Sources/MGO/Features/Dashboard/DashboardCoordinator.swift
@@ -13,7 +13,7 @@ protocol DashboardCoordinatorProtocol: Coordinator, ObservableObject {
 	/// Get a View for the State
 	/// - Parameter state: the Dashboard Coordination State
 	/// - Returns: A view for that state
-	func viewState(for: DashboardCoordination.State?) -> Body
+	@MainActor func view(for: DashboardCoordination.State?) -> Body
 	
 	/// The selected tab
 	var selectedTab: Int { get set }
@@ -101,7 +101,7 @@ class DashboardCoordinator: DashboardCoordinatorProtocol {
 	/// Get a View for the State
 	/// - Parameter state: the DashboardCoordination State
 	/// - Returns: A view for that state
-	@MainActor @ViewBuilder func viewState(for state: DashboardCoordination.State?) -> some View {
+	@MainActor @ViewBuilder func view(for state: DashboardCoordination.State?) -> some View {
 		
 		switch state {
 			
