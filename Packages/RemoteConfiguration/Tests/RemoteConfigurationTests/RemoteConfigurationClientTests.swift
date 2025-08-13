1e35c9816313460118d266ebe34ed8d9ae46ea63
Rool Paap <rool@rool.nl>
2025-08-13 13:53:44 +0200

    test: fixed test files

--- Packages/RemoteConfiguration/Tests/RemoteConfigurationTests/RemoteConfigurationClientTests.swift
+++ Packages/RemoteConfiguration/Tests/RemoteConfigurationTests/RemoteConfigurationClientTests.swift
@@ -4,105 +4,34 @@
  */
 
 @testable import RemoteConfiguration
-import FileStorage
 import MGOTest
 
-final class RemoteConfigurationRepositoryTests: XCTestCase {
+final class RemoteConfigurationClientTests: XCTestCase {
 	
-	private var storageSpy: FileStorageSpy!
-	private var clientSpy: RemoteConfigurationClientSpy!
-	private var sut: RemoteConfigurationRepository!
+	private var sut: RemoteConfigurationClient!
+	
+	override func tearDown() {
+		super.tearDown()
+		HTTPStubs.removeAllStubs()
+	}
 	
 	override func setUpWithError() throws {
 		
 		try super.setUpWithError()
-		let serverUrl = try XCTUnwrap(URL(string: "https://example.com"))
-		storageSpy = FileStorageSpy()
-		clientSpy = RemoteConfigurationClientSpy(serverUrl: serverUrl)
-		sut = RemoteConfigurationRepository(storage: storageSpy, apiClient: clientSpy)
+		let serverUrl = try XCTUnwrap(URL(string: "https://example.com/v1/mgo"))
+		sut = try XCTUnwrap(RemoteConfigurationClient(serverUrl: serverUrl))
 	}
 	
-	override func tearDown() {
-
-		super.tearDown()
-		sut.wipePersistedData()
-	}
-	
-	func test_init() {
+	func test_fetchRemoteConfig() async throws {
 		
 		// Given
-		
+		stub(condition: isPath("/v1/mgo/config")) { _ in
+			return HTTPStubsResponse(jsonObject: ["iosMinimumVersion": "1.2.3"], statusCode: 200, headers: nil)
+		}
 		// When
-		let config = sut.storedConfiguration
+		let remoteConfig = try await sut.fetchRemoteConfig()
 		
 		// Then
-		expect(config) == RemoteConfig.fallback
-	}
-
-	func test_fetchFromAPI() async {
-		
-		// Given
-		clientSpy.stubbedRemoteConfiguration = RemoteConfig(iosMinimumVersion: "fetchFromApi")
-		
-		// When
-		await sut.fetchAndUpdateObservers()
-		
-		// Then
-		await expect(self.sut.storedConfiguration)
-			.toEventually(equal(clientSpy.stubbedRemoteConfiguration))
-	}
-	
-	func test_fetchFromStorage() async throws {
-		
-		// Given
-		let error = NSError(
-			domain: "RemoteConfigurationRepositoryTests",
-			code: 404
-		)
-		clientSpy.stubbedError = error // Error from API
-		let config = RemoteConfig(iosMinimumVersion: "fetchFromStorage")
-		let encoded = try JSONEncoder().encode(config)
-		storageSpy.stubbedReadResult = encoded // Config from storage
-		
-		// When
-		await sut.fetchAndUpdateObservers()
-		
-		// Then
-		await expect(self.sut.storedConfiguration)
-			.toEventually(equal(config))
-	}
-	
-	func test_fetchFromStorage_noClient() async throws {
-		
-		// Given
-		let config = RemoteConfig(iosMinimumVersion: "fetchFromStorage")
-		let encoded = try JSONEncoder().encode(config)
-		storageSpy.stubbedReadResult = encoded // Config from storage
-		sut = RemoteConfigurationRepository(storage: storageSpy, apiClient: nil)
-		
-		// When
-		await sut.fetchAndUpdateObservers()
-		
-		// Then
-		await expect(self.sut.storedConfiguration)
-			.toEventually(equal(config))
-	}
-	
-	func test_fetchFromFallBack() async throws {
-		
-		// Given
-		let error = NSError(
-			domain: "RemoteConfigurationRepositoryTests",
-			code: 404
-		)
-		clientSpy.stubbedError = error // Error from API
-		storageSpy.stubbedReadResult = Data() // No config from storage
-		
-		// When
-		await sut.fetchAndUpdateObservers()
-		
-		// Then
-		await expect(self.sut.storedConfiguration)
-			.toEventually(equal(RemoteConfig.fallback))
+		expect(remoteConfig.iosMinimumVersion) == "1.2.3"
 	}
 }
--- Packages/RemoteConfiguration/Tests/RemoteConfigurationTests/RemoteConfigurationRepositoryTests.swift
+++ Packages/RemoteConfiguration/Tests/RemoteConfigurationTests/RemoteConfigurationRepositoryTests.swift
@@ -4,34 +4,105 @@
  */
 
 @testable import RemoteConfiguration
+import FileStorage
 import MGOTest
 
-final class RemoteConfigurationClientTests: XCTestCase {
+final class RemoteConfigurationRepositoryTests: XCTestCase {
 	
-	private var sut: RemoteConfigurationClient!
-	
-	override func tearDown() {
-		super.tearDown()
-		HTTPStubs.removeAllStubs()
-	}
+	private var storageSpy: FileStorageSpy!
+	private var clientSpy: RemoteConfigurationClientSpy!
+	private var sut: RemoteConfigurationRepository!
 	
 	override func setUpWithError() throws {
 		
 		try super.setUpWithError()
-		let serverUrl = try XCTUnwrap(URL(string: "https://example.com/v1/mgo"))
-		sut = try XCTUnwrap(RemoteConfigurationClient(serverUrl: serverUrl))
+		let serverUrl = try XCTUnwrap(URL(string: "https://example.com"))
+		storageSpy = FileStorageSpy()
+		clientSpy = RemoteConfigurationClientSpy(serverUrl: serverUrl)
+		sut = RemoteConfigurationRepository(storage: storageSpy, apiClient: clientSpy)
 	}
 	
-	func test_fetchRemoteConfig() async throws {
+	override func tearDown() {
+
+		super.tearDown()
+		sut.wipePersistedData()
+	}
+	
+	func test_init() {
 		
 		// Given
-		stub(condition: isPath("/v1/mgo/config")) { _ in
-			return HTTPStubsResponse(jsonObject: ["iosMinimumVersion": "1.2.3"], statusCode: 200, headers: nil)
-		}
+		
 		// When
-		let remoteConfig = try await sut.fetchRemoteConfig()
+		let config = sut.storedConfiguration
 		
 		// Then
-		expect(remoteConfig.iosMinimumVersion) == "1.2.3"
+		expect(config) == RemoteConfig.fallback
+	}
+
+	func test_fetchFromAPI() async {
+		
+		// Given
+		clientSpy.stubbedRemoteConfiguration = RemoteConfig(iosMinimumVersion: "fetchFromApi")
+		
+		// When
+		await sut.fetchAndUpdateObservers()
+		
+		// Then
+		await expect(self.sut.storedConfiguration)
+			.toEventually(equal(clientSpy.stubbedRemoteConfiguration))
+	}
+	
+	func test_fetchFromStorage() async throws {
+		
+		// Given
+		let error = NSError(
+			domain: "RemoteConfigurationRepositoryTests",
+			code: 404
+		)
+		clientSpy.stubbedError = error // Error from API
+		let config = RemoteConfig(iosMinimumVersion: "fetchFromStorage")
+		let encoded = try JSONEncoder().encode(config)
+		storageSpy.stubbedReadResult = encoded // Config from storage
+		
+		// When
+		await sut.fetchAndUpdateObservers()
+		
+		// Then
+		await expect(self.sut.storedConfiguration)
+			.toEventually(equal(config))
+	}
+	
+	func test_fetchFromStorage_noClient() async throws {
+		
+		// Given
+		let config = RemoteConfig(iosMinimumVersion: "fetchFromStorage")
+		let encoded = try JSONEncoder().encode(config)
+		storageSpy.stubbedReadResult = encoded // Config from storage
+		sut = RemoteConfigurationRepository(storage: storageSpy, apiClient: nil)
+		
+		// When
+		await sut.fetchAndUpdateObservers()
+		
+		// Then
+		await expect(self.sut.storedConfiguration)
+			.toEventually(equal(config))
+	}
+	
+	func test_fetchFromFallBack() async throws {
+		
+		// Given
+		let error = NSError(
+			domain: "RemoteConfigurationRepositoryTests",
+			code: 404
+		)
+		clientSpy.stubbedError = error // Error from API
+		storageSpy.stubbedReadResult = Data() // No config from storage
+		
+		// When
+		await sut.fetchAndUpdateObservers()
+		
+		// Then
+		await expect(self.sut.storedConfiguration)
+			.toEventually(equal(RemoteConfig.fallback))
 	}
 }
