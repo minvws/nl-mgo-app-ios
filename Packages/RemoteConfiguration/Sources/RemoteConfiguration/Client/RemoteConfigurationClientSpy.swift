7d7b7065740a94c0168836eef8ad938f73112648
Rool Paap <rool@rool.nl>
2025-08-13 13:53:30 +0200

    feat: added auth middleware to remote config client

--- Packages/RemoteConfiguration/Sources/RemoteConfiguration/Client/RemoteConfigurationClientSpy.swift
+++ Packages/RemoteConfiguration/Sources/RemoteConfiguration/Client/RemoteConfigurationClientSpy.swift
@@ -10,6 +10,8 @@ import Foundation
 public class RemoteConfigurationClientSpy: RemoteConfigurationClientProtocol {
 
 	public required init(serverUrl: Foundation.URL) { /* Public initializer needed for public access */ }
+	
+	public required init(serverUrl: Foundation.URL, username: String, password: String) { /* Public initializer needed for public access */ }
 
 	public var invokedFetchRemoteConfig = false
 	public var invokedFetchRemoteConfigCount = 0
