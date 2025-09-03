fde5760c8f72c13baee881109e9ea69be7883bff
Rool Paap <rool@rool.nl>
2025-09-03 08:52:26 +0200

    refactor: removed dedicated health category views

--- Sources/MGO/Features/Healthcare/HealthcareCoordinator.swift
+++ Sources/MGO/Features/Healthcare/HealthcareCoordinator.swift
@@ -418,72 +418,33 @@ class HealthcareCoordinator: HealthcareCoordinatorProtocol {
 	/// - Parameter category: the health category
 	/// - Parameter organization: optional healthcare organization
 	/// - Returns: A view for that state
-	@MainActor @ViewBuilder private func viewState(for category: SharedHealthCategories.Category, organization: MgoOrganization? = nil) -> some View {
+	@MainActor @ViewBuilder private func viewState(
+		for category: SharedHealthCategories.Category,
+		organization: MgoOrganization? = nil
+	) -> some View {
 		
-		switch category.id {
-			case "alerts":
-				HealthCategoryView(viewModel: AlertsHealthCategoryViewModel(coordinator: self, category: category, organization: organization))
-			
-			case "allergies":
-				HealthCategoryView(viewModel: AllergiesHealthCategoryViewModel(coordinator: self, category: category, organization: organization))
-
-			case "lab_results":
-				HealthCategoryView(viewModel: LabResultsHealthCategoryViewModel(coordinator: self, category: category, organization: organization))
-			
-			case "vaccinations":
-				HealthCategoryView(viewModel: VaccinationsHealthCategoryViewModel(coordinator: self, category: category, organization: organization))
-			
-			default:
-				Text("Todo, HealthCategoryView for \(category.id)")
-		
-//		switch category {
-//			case HealthCategories.Category.medication:
-//				HealthCategoryView(viewModel: MedicationHealthCategoryViewModel(coordinator: self, organization: organization))
-//				
-//			case HealthCategories.Category.measurements:
-//				HealthCategoryView(viewModel: MeasurementsHealthCategoryViewModel(coordinator: self, organization: organization))
-//				
-//			case HealthCategories.Category.labResults:
-//				HealthCategoryView(viewModel: LabResultsHealthCategoryViewModel(coordinator: self, organization: organization))
-//				
-//			case HealthCategories.Category.allergies:
-//				HealthCategoryView(viewModel: AllergiesHealthCategoryViewModel(coordinator: self, organization: organization))
-//				
-//			case HealthCategories.Category.treatments:
-//				HealthCategoryView(viewModel: TreatmentsHealthCategoryViewModel(coordinator: self, organization: organization))
-//				
-//			case HealthCategories.Category.appointments:
-//				HealthCategoryView(viewModel: AppointmentsHealthCategoryViewModel(coordinator: self, organization: organization))
-//				
-//			case HealthCategories.Category.vaccinations:
-//				HealthCategoryView(viewModel: VaccinationsHealthCategoryViewModel(coordinator: self, organization: organization))
-//				
-//			case HealthCategories.Category.documents:
-//				HealthCategoryView(viewModel: DocumentsHealthCategoryViewModel(coordinator: self, organization: organization))
-//				
-//			case HealthCategories.Category.medicalComplaints:
-//				HealthCategoryView(viewModel: ComplaintsHealthCategoryViewModel(coordinator: self, organization: organization))
-//				
-//			case HealthCategories.Category.personalDetails:
-//				HealthCategoryView(viewModel: PatientHealthCategoryViewModel(coordinator: self, organization: organization))
-//				
-//			case HealthCategories.Category.alerts:
-//				HealthCategoryView(viewModel: AlertsHealthCategoryViewModel(coordinator: self, organization: organization))
-//				
-//			case HealthCategories.Category.payment:
-//				HealthCategoryView(viewModel: PaymentHealthCategoryViewModel(coordinator: self, organization: organization))
-//				
-//			case HealthCategories.Category.plans:
-//				HealthCategoryView(viewModel: PlansHealthCategoryViewModel(coordinator: self, organization: organization))
-//				
-//			case HealthCategories.Category.medicalDevices:
-//				HealthCategoryView(viewModel: DevicesHealthCategoryViewModel(coordinator: self, organization: organization))
-//				
-//			case HealthCategories.Category.mentalWellbeing:
-//				HealthCategoryView(viewModel: MentalStatusHealthCategoryViewModel(coordinator: self, organization: organization))
-//				
-//			case HealthCategories.Category.lifestyle:
-//				HealthCategoryView(viewModel: LifestyleHealthCategoryViewModel(coordinator: self, organization: organization))
+		if category.id == "documents" {
+			// Documents has an override method for sortRecords.
+			HealthCategoryView(
+				viewModel: DocumentsHealthCategoryViewModel(
+					coordinator: self,
+					category: category,
+					organization: organization
+				)
+			)
+		} else {
+			if let translations = HealthCategoryViewTranslationsFactory.makeTranslations(for: category) {
+				HealthCategoryView(
+					viewModel: HealthCategoryViewModel(
+						coordinator: self,
+						category: category,
+						organization: organization,
+						translations: translations
+					)
+				)
+			} else {
+				EmptyView()
+			}
 		}
 	}
 }
