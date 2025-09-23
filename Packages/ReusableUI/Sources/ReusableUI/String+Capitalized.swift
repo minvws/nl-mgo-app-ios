389164bbdc3d182753f252ccf8e6997237b38cb0
Rool Paap <rool@rool.nl>
2025-09-23 13:31:41 +0200

    test: capitalize first char tests

--- Packages/ReusableUI/Sources/ReusableUI/String+Capitalized.swift
+++ Packages/ReusableUI/Sources/ReusableUI/String+Capitalized.swift
@@ -9,8 +9,4 @@ public extension String {
 	func capitalizingFirstLetter() -> String {
 		return prefix(1).uppercased() + self.lowercased().dropFirst()
 	}
-	
-	mutating func capitalizeFirstLetter() {
-		self = self.capitalizingFirstLetter()
-	}
 }
