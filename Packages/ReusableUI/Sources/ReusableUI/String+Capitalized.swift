dd1756767ea93e6bbd3baaf93acaf8ddaf548637
Rool Paap <rool@rool.nl>
2025-09-23 09:55:15 +0200

    style: updated layout patient friendly terms modal

---
+++ Packages/ReusableUI/Sources/ReusableUI/String+Capitalized.swift
@@ -0,0 +1,16 @@
+/*
+ *  SPDX-FileCopyrightText: 2025 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
+ *  SPDX-License-Identifier: EUPL-1.2
+ */
+
+import SwiftUI
+
+public extension String {
+	func capitalizingFirstLetter() -> String {
+		return prefix(1).uppercased() + self.lowercased().dropFirst()
+	}
+	
+	mutating func capitalizeFirstLetter() {
+		self = self.capitalizingFirstLetter()
+	}
+}
