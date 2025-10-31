8fa9566e89e39c22d0faf27caca031d513bdfaa8
Rool Paap <rool@rool.nl>
2025-10-31 11:49:27 +0100

    style: introducing typography modifier

--- Packages/RijksoverheidFont/Sources/RijksoverheidFont/RijksoverheidViewModifier.swift
+++ Packages/RijksoverheidFont/Sources/RijksoverheidFont/RijksoverheidViewModifier.swift
@@ -26,6 +26,24 @@ extension Font.TextStyle {
 		}
 	}
 	
+	/// The default point size for text styles
+	public var pointSize2: CGFloat {
+		switch self {
+			case .largeTitle: isiPhoneSE ? 32 : 34
+			case .title: isiPhoneSE ? 26 : 28
+			case .title2: 22
+			case .title3: 20
+			case .headline: 20
+			case .subheadline: 18
+			case .body: 18
+			case .callout: 16
+			case .footnote: 14
+			case .caption: 12
+			case .caption2: 12
+			@unknown default: 18
+		}
+	}
+	
 	/// Do we run on a very small iPhoneSE ?
 	private var isiPhoneSE: Bool {
 		Device.current == .iPhoneSE || Device.current == .simulator(.iPhoneSE)
