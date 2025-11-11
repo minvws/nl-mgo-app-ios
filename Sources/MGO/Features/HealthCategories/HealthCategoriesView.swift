c36c20a946d31aa2f6c45e29a9e9b174d5886d7a
Rool Paap <rool@rool.nl>
2025-11-11 16:54:39 +0100

    style: fixed cut off subheading

--- Sources/MGO/Features/HealthCategories/HealthCategoriesView.swift
+++ Sources/MGO/Features/HealthCategories/HealthCategoriesView.swift
@@ -366,6 +366,7 @@ struct HealthCategoriesView: View {
 		enum List {
 			static let rowInset = EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
 			static let headerInset = EdgeInsets(top: 24, leading: 0, bottom: 12, trailing: 0)
+			static let alternativeInset = EdgeInsets(top: 12, leading: 0, bottom: 12, trailing: 0)
 			static let spacing: CGFloat = 4
 			static let padding: CGFloat = 8
 			static let demoSpacing: CGFloat = 16
@@ -496,12 +497,23 @@ struct HealthCategoriesView: View {
 					.accessibilityAddTraits(.isHeader)
 			}
 			.listRowBackground(Color.clear)
-			.listRowInsets(ViewTraits.List.headerInset)
+			.listRowInsets(listHeaderInset(for: mainCategory))
 			
 			Section {
 				ForEach(filteredCategories) { categoryView($0) }
 			}
 		}
+	}
+	
+	/// Calculate the header inset (the first header on the organization page has a smaller inset)
+	/// - Parameter mainCategory: the main category
+	/// - Returns: the inset
+	func listHeaderInset(for mainCategory: SharedHealthCategories.MainCategory) -> EdgeInsets {
+		
+		if !viewModel.state.canTitleCollapse && mainCategory == viewModel.state.mainCategories.first {
+			return ViewTraits.List.alternativeInset
+		}
+		return ViewTraits.List.headerInset
 	}
 	
 	/// View for a category
@@ -534,6 +546,7 @@ struct HealthCategoriesView: View {
 			.padding(.vertical, ViewTraits.List.padding)
 			.accessibilityIdentifier(category.id)
 			.contentShape(Rectangle())
+			
 		}
 	}
 	
@@ -545,7 +558,7 @@ struct HealthCategoriesView: View {
 			VStack(spacing: ViewTraits.General.padding) {
 				heading()
 				subHeading()
-					.padding(.bottom, viewModel.state.canTitleCollapse ? 0 : ViewTraits.General.padding / 2)
+					.padding(.bottom, ViewTraits.General.padding)
 			}
 		}
 		.listRowBackground(Color.clear)
