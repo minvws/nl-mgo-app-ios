2d9ca807935ccaf248c55303fae865b5e2ff418d
Rool Paap <rool@rool.nl>
2025-11-19 11:43:06 +0100

    style: fixing margins on older iOS version

--- Sources/MGO/Features/HealthCategory/HealthCategoryView.swift
+++ Sources/MGO/Features/HealthCategory/HealthCategoryView.swift
@@ -458,7 +458,6 @@ struct HealthCategoryView: View {
 			}
 		}
 		.backport.scrollContentBackground(.hidden)
-		.environment(\.defaultMinListHeaderHeight, ViewTraits.General.padding / 2)
 		.navigationBarBackButtonHidden()
 		.navigationBarItems(leading: BackButton("overview.heading") {
 			viewModel.reduce(.backButtonPressed)
@@ -504,6 +503,7 @@ struct HealthCategoryView: View {
 				listOverviewBlocks(list: filterList(list))
 					.backport.listSectionSpacing(8)
 					.backport.contentMargins(0)
+					.environment(\.defaultMinListHeaderHeight, ViewTraits.General.padding / 2)
 					
 			} else {
 				noItems()
