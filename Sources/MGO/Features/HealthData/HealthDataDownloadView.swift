ee3b080d72fbc9c6f3350d7596491dc391ccbc4e
Rool Paap <rool@rool.nl>
2025-11-20 13:28:36 +0100

    style: fix background bottom PDF preview

--- Sources/MGO/Features/HealthData/HealthDataDownloadView.swift
+++ Sources/MGO/Features/HealthData/HealthDataDownloadView.swift
@@ -217,6 +217,8 @@ struct HealthDataDownloadView: View {
 	
 	@State private var failedToOpenPreview: Bool = false
 	
+	@Environment(\.colorScheme) var colorScheme
+	
 	/// Magic Numbers
 	private struct ViewTraits {
 		
@@ -224,6 +226,10 @@ struct HealthDataDownloadView: View {
 			static let horizontal: CGFloat = 16
 			static let vertical: CGFloat = 24
 			static let spacing: CGFloat = 8
+		}
+		enum Preview {
+			static let lightBackgroundColor: Color = Color(hex: "FCFCFC")
+			static let dardBackgroundColor: Color = Color(hex: "E7E7E7")
 		}
 	}
 	
@@ -252,7 +258,9 @@ struct HealthDataDownloadView: View {
 					}
 					.sheet(isPresented: $viewModel.showPreview) {
 						DocumentPreviewController($viewModel.showPreview, failedToOpen: $failedToOpenPreview, url: documentUrl)
-							.background(theme.backgrounds.primary)
+							.background(
+								colorScheme == .light ? ViewTraits.Preview.lightBackgroundColor : ViewTraits.Preview
+									.dardBackgroundColor)
 							.interactiveDismissDisabled(true)
 					}
 					.onChange(of: failedToOpenPreview) { newValue in
