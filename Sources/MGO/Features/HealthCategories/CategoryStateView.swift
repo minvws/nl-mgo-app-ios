d245a15947649ac571c697c4fb1c67b64467da42
Rool Paap <rool@rool.nl>
2025-11-27 11:28:08 +0100

    style: updated favorites and healthcategory row with error state

---
+++ Sources/MGO/Features/HealthCategories/CategoryStateView.swift
@@ -0,0 +1,56 @@
+/*
+ *  SPDX-FileCopyrightText: 2025 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
+ *  SPDX-License-Identifier: EUPL-1.2
+ */
+
+import MGOUI
+
+struct CategoryStateView: View {
+	
+	/// The Theme
+	@Environment(\.theme) var theme
+	
+	/// The state
+	var state: CategoryState
+	
+	/// Magic Numbers
+	private struct ViewTraits {
+		enum Accessory {
+			static let size: CGFloat = 22
+		}
+	}
+	
+	/// The category state view
+	var body: some View {
+		
+		switch state {
+			case .loaded:
+				Image(ImageResource.Icon.chevron)
+					.foregroundStyle(theme.symbols.secondary)
+					.frame(
+						width: ViewTraits.Accessory.size,
+						height: ViewTraits.Accessory.size
+					)
+				
+			case .loading:
+				ProgressView()
+					.progressViewStyle(.circular)
+					.frame(
+						width: ViewTraits.Accessory.size,
+						height: ViewTraits.Accessory.size
+					)
+					.tint(theme.symbols.secondary)
+				
+			case .error:
+				Image(ImageResource.Icon.syncProblem)
+					.foregroundStyle(theme.symbols.primary)
+					.frame(
+						width: ViewTraits.Accessory.size,
+						height: ViewTraits.Accessory.size
+					)
+				
+			default:
+				Spacer()
+		}
+	}
+}
