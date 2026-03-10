b35c0ab6205f7709574b0a1c08c31adc93a961b1
Rool Paap <rool@rool.nl>
2026-03-10 08:59:08 +0100

    fix: reverted extraction

--- Sources/MGO/Features/Splash/SnapshotView.swift
+++ Sources/MGO/Features/Splash/SnapshotView.swift
@@ -21,42 +21,38 @@ struct SnapshotView: View {
 		}
 	}
 	
-	fileprivate func extractedFunc() -> VStack<TupleView<(Spacer, some View)>> {
-		return VStack {
-			
-			Spacer()
-			
-			HStack(spacing: ViewTraits.General.spacing) {
-				
-				Spacer()
-				
-				ProgressView()
-					.tint(theme.labels.primary.opacity(ViewTraits.General.opacity))
-					.accessibilityHidden(true)
-				
-				Text("common.loading")
-					.foregroundStyle(theme.labels.primary.opacity(ViewTraits.General.opacity))
-					.typography(.bodyMedium)
-				
-				Spacer()
-			}
-			.offset(y: -geometry.size.height / 4)
-		}
-	}
-	
 	var body: some View {
 		GeometryReader { geometry in
 			ZStack {
-				
+
 				theme.backgrounds.primary
 					.frame(maxWidth: .infinity, maxHeight: .infinity)
-				
+
 				Image(ImageResource.splashLogo)
 					.accessibilityLabel("common.app_name")
 					.accessibilityIdentifier("common.app_name")
-				
+
 				if showSpinner {
-					extractedFunc()
+					VStack {
+
+						Spacer()
+
+						HStack(spacing: ViewTraits.General.spacing) {
+
+							Spacer()
+
+							ProgressView()
+								.tint(theme.labels.primary.opacity(ViewTraits.General.opacity))
+								.accessibilityHidden(true)
+
+							Text("common.loading")
+								.foregroundStyle(theme.labels.primary.opacity(ViewTraits.General.opacity))
+								.typography(.bodyMedium)
+
+							Spacer()
+						}
+						.offset(y: -geometry.size.height / 4)
+					}
 				}
 			}
 		}
