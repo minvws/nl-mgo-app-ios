09f34a357cf4f148755b7fcc747357f06192febf
Rool Paap <rool@rool.nl>
2026-04-17 13:48:06 +0200

    test: updated pdf export flow tests

--- MGOUITests/Robots/ViewRobots/HealthExport/HealthExportRobot.swift
+++ MGOUITests/Robots/ViewRobots/HealthExport/HealthExportRobot.swift
@@ -1,15 +1,15 @@
 /*
- *  SPDX-FileCopyrightText: 2025 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
+ *  SPDX-FileCopyrightText: 2026 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
  *  SPDX-License-Identifier: EUPL-1.2
  */
 
 import XCTest
 
 @MainActor class HealthExportRobot: Robot {
-
+	
 	/// The app to test
 	let app: XCUIApplication
-
+	
 	/// Create a Health Export Robot
 	/// - Parameter application: the application to test
 	init(_ application: XCUIApplication) {
@@ -19,30 +19,36 @@ import XCTest
 			"Expected 'HealthExportRobot' screen, but it didn't appear"
 		)
 	}
-
+	
 	// MARK: - Elements
-
+	
 	private var closeButton: XCUIElement {
 		app.buttons["export_pdf.close"]
 	}
-
+	
 	private var shareButton: XCUIElement {
 		app.buttons["export_pdf.share"]
 	}
-
+	
 	// MARK: - Validations
-
+	
 	@discardableResult
 	func verifyShareButtonExists() -> Self {
 		XCTAssertTrue(shareButton.waitForExistence(timeout: timeOut))
 		return self
 	}
-
+	
 	// MARK: - Interactions
-
+	
 	@discardableResult
 	func close() -> HealthCategoryRobot {
 		closeButton.tap()
 		return HealthCategoryRobot(app)
+	}
+	
+	@discardableResult
+	func closeHealthUISchema() -> HealthUISchemaRobot {
+		closeButton.tap()
+		return HealthUISchemaRobot(app)
 	}
 }
